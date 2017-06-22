StatCan/dataviz-components

commit ccb023fa4e19c939b9fa5416eea0ba5f73d4b1a8

Fixed the labels being added on each update

dist/area.js
@@ -130,30 +130,32 @@ this.areaChart = function(svg, settings) {
         .attr("class", "x axis")
         .attr("aria-hidden", "true")
         .attr("transform", "translate(0," + innerHeight + ")");
+
+        xAxisObj.call(xAxis)
+          .append("text")
+            .attr("class", "chart-label")
+            .attr("fill", "#000")
+            .attr("x", innerWidth)
+            .attr("dy", "-0.5em")
+            .attr("text-anchor", "end")
+            .text(settings.x.label);
       }
-      xAxisObj.call(xAxis)
-        .append("text")
-          .attr("class", "chart-label")
-          .attr("fill", "#000")
-          .attr("x", innerWidth)
-          .attr("dy", "-0.5em")
-          .attr("text-anchor", "end")
-          .text(settings.x.label);
 
       if (yAxisObj.empty()) {
         yAxisObj = chartInner.append("g")
           .attr("class", "y axis")
           .attr("aria-hidden", "true");
+
+        yAxisObj.call(yAxis)
+          .append("text")
+            .attr("class", "chart-label")
+            .attr("fill", "#000")
+            .attr("y", "0")
+            .attr("transform", "rotate(-90)")
+            .attr("dy", "1.5em")
+            .attr("text-anchor", "end")
+            .text(settings.y.label);
       }
-      yAxisObj.call(yAxis)
-        .append("text")
-          .attr("class", "chart-label")
-          .attr("fill", "#000")
-          .attr("y", "0")
-          .attr("transform", "rotate(-90)")
-          .attr("dy", "1.5em")
-          .attr("text-anchor", "end")
-          .text(settings.y.label);
     },
     drawTable = function() {
       var sett = this.settings,

dist/line.js
@@ -127,30 +127,32 @@ this.lineChart = function(svg, settings) {
         .attr("class", "x axis")
         .attr("aria-hidden", "true")
         .attr("transform", "translate(0," + innerHeight + ")");
+
+        xAxisObj.call(xAxis)
+          .append("text")
+            .attr("class", "chart-label")
+            .attr("fill", "#000")
+            .attr("x", innerWidth)
+            .attr("dy", "-0.5em")
+            .attr("text-anchor", "end")
+            .text(settings.x.label);
       }
-      xAxisObj.call(xAxis)
-        .append("text")
-          .attr("class", "chart-label")
-          .attr("fill", "#000")
-          .attr("x", innerWidth)
-          .attr("dy", "-0.5em")
-          .attr("text-anchor", "end")
-          .text(settings.x.label);
 
       if (yAxisObj.empty()) {
         yAxisObj = chartInner.append("g")
           .attr("class", "y axis")
           .attr("aria-hidden", "true");
+
+        yAxisObj.call(yAxis)
+          .append("text")
+            .attr("class", "chart-label")
+            .attr("fill", "#000")
+            .attr("y", "0")
+            .attr("transform", "rotate(-90)")
+            .attr("dy", "1.5em")
+            .attr("text-anchor", "end")
+            .text(settings.y.label);
       }
-      yAxisObj.call(yAxis)
-        .append("text")
-          .attr("class", "chart-label")
-          .attr("fill", "#000")
-          .attr("y", "0")
-          .attr("transform", "rotate(-90)")
-          .attr("dy", "1.5em")
-          .attr("text-anchor", "end")
-          .text(settings.y.label);
     },
     drawTable = function() {
       var sett = this.settings,

dist/scatter.js
@@ -197,30 +197,32 @@ this.scatterChart = function(svg, settings) {
         .attr("class", "x axis")
         .attr("aria-hidden", "true")
         .attr("transform", "translate(0," + innerHeight + ")");
+
+        xAxisObj.call(xAxis)
+          .append("text")
+            .attr("class", "chart-label")
+            .attr("fill", "#000")
+            .attr("x", innerWidth)
+            .attr("dy", "-0.5em")
+            .attr("text-anchor", "end")
+            .text(settings.x.label);
       }
-      xAxisObj.call(xAxis)
-        .append("text")
-          .attr("class", "chart-label")
-          .attr("fill", "#000")
-          .attr("x", innerWidth)
-          .attr("dy", "-0.5em")
-          .attr("text-anchor", "end")
-          .text(settings.x.label);
 
       if (yAxisObj.empty()) {
         yAxisObj = chartInner.append("g")
           .attr("class", "y axis")
           .attr("aria-hidden", "true");
+
+        yAxisObj.call(yAxis)
+          .append("text")
+            .attr("class", "chart-label")
+            .attr("fill", "#000")
+            .attr("y", "0")
+            .attr("transform", "rotate(-90)")
+            .attr("dy", "1.5em")
+            .attr("text-anchor", "end")
+            .text(settings.y.label);
       }
-      yAxisObj.call(yAxis)
-        .append("text")
-          .attr("class", "chart-label")
-          .attr("fill", "#000")
-          .attr("y", "0")
-          .attr("transform", "rotate(-90)")
-          .attr("dy", "1.5em")
-          .attr("text-anchor", "end")
-          .text(settings.y.label);
     },
     drawTable = function() {
       var sett = this.settings,

src/area/area.js
@@ -130,30 +130,32 @@ this.areaChart = function(svg, settings) {
         .attr("class", "x axis")
         .attr("aria-hidden", "true")
         .attr("transform", "translate(0," + innerHeight + ")");
+
+        xAxisObj.call(xAxis)
+          .append("text")
+            .attr("class", "chart-label")
+            .attr("fill", "#000")
+            .attr("x", innerWidth)
+            .attr("dy", "-0.5em")
+            .attr("text-anchor", "end")
+            .text(settings.x.label);
       }
-      xAxisObj.call(xAxis)
-        .append("text")
-          .attr("class", "chart-label")
-          .attr("fill", "#000")
-          .attr("x", innerWidth)
-          .attr("dy", "-0.5em")
-          .attr("text-anchor", "end")
-          .text(settings.x.label);
 
       if (yAxisObj.empty()) {
         yAxisObj = chartInner.append("g")
           .attr("class", "y axis")
           .attr("aria-hidden", "true");
+
+        yAxisObj.call(yAxis)
+          .append("text")
+            .attr("class", "chart-label")
+            .attr("fill", "#000")
+            .attr("y", "0")
+            .attr("transform", "rotate(-90)")
+            .attr("dy", "1.5em")
+            .attr("text-anchor", "end")
+            .text(settings.y.label);
       }
-      yAxisObj.call(yAxis)
-        .append("text")
-          .attr("class", "chart-label")
-          .attr("fill", "#000")
-          .attr("y", "0")
-          .attr("transform", "rotate(-90)")
-          .attr("dy", "1.5em")
-          .attr("text-anchor", "end")
-          .text(settings.y.label);
     },
     drawTable = function() {
       var sett = this.settings,

src/line/line.js
@@ -127,30 +127,32 @@ this.lineChart = function(svg, settings) {
         .attr("class", "x axis")
         .attr("aria-hidden", "true")
         .attr("transform", "translate(0," + innerHeight + ")");
+
+        xAxisObj.call(xAxis)
+          .append("text")
+            .attr("class", "chart-label")
+            .attr("fill", "#000")
+            .attr("x", innerWidth)
+            .attr("dy", "-0.5em")
+            .attr("text-anchor", "end")
+            .text(settings.x.label);
       }
-      xAxisObj.call(xAxis)
-        .append("text")
-          .attr("class", "chart-label")
-          .attr("fill", "#000")
-          .attr("x", innerWidth)
-          .attr("dy", "-0.5em")
-          .attr("text-anchor", "end")
-          .text(settings.x.label);
 
       if (yAxisObj.empty()) {
         yAxisObj = chartInner.append("g")
           .attr("class", "y axis")
           .attr("aria-hidden", "true");
+
+        yAxisObj.call(yAxis)
+          .append("text")
+            .attr("class", "chart-label")
+            .attr("fill", "#000")
+            .attr("y", "0")
+            .attr("transform", "rotate(-90)")
+            .attr("dy", "1.5em")
+            .attr("text-anchor", "end")
+            .text(settings.y.label);
       }
-      yAxisObj.call(yAxis)
-        .append("text")
-          .attr("class", "chart-label")
-          .attr("fill", "#000")
-          .attr("y", "0")
-          .attr("transform", "rotate(-90)")
-          .attr("dy", "1.5em")
-          .attr("text-anchor", "end")
-          .text(settings.y.label);
     },
     drawTable = function() {
       var sett = this.settings,

src/scatter/scatter.js
@@ -197,30 +197,32 @@ this.scatterChart = function(svg, settings) {
         .attr("class", "x axis")
         .attr("aria-hidden", "true")
         .attr("transform", "translate(0," + innerHeight + ")");
+
+        xAxisObj.call(xAxis)
+          .append("text")
+            .attr("class", "chart-label")
+            .attr("fill", "#000")
+            .attr("x", innerWidth)
+            .attr("dy", "-0.5em")
+            .attr("text-anchor", "end")
+            .text(settings.x.label);
       }
-      xAxisObj.call(xAxis)
-        .append("text")
-          .attr("class", "chart-label")
-          .attr("fill", "#000")
-          .attr("x", innerWidth)
-          .attr("dy", "-0.5em")
-          .attr("text-anchor", "end")
-          .text(settings.x.label);
 
       if (yAxisObj.empty()) {
         yAxisObj = chartInner.append("g")
           .attr("class", "y axis")
           .attr("aria-hidden", "true");
+
+        yAxisObj.call(yAxis)
+          .append("text")
+            .attr("class", "chart-label")
+            .attr("fill", "#000")
+            .attr("y", "0")
+            .attr("transform", "rotate(-90)")
+            .attr("dy", "1.5em")
+            .attr("text-anchor", "end")
+            .text(settings.y.label);
       }
-      yAxisObj.call(yAxis)
-        .append("text")
-          .attr("class", "chart-label")
-          .attr("fill", "#000")
-          .attr("y", "0")
-          .attr("transform", "rotate(-90)")
-          .attr("dy", "1.5em")
-          .attr("text-anchor", "end")
-          .text(settings.y.label);
     },
     drawTable = function() {
       var sett = this.settings,

commit 5899b85f63b9d5660b79ab141c1785e76b5bdd6a

Fixed the axis not being updated
Regression from ccb023fa4e19c939b9fa5416eea0ba5f73d4b1a8

dist/area.js
@@ -131,7 +131,7 @@ this.areaChart = function(svg, settings) {
         .attr("aria-hidden", "true")
         .attr("transform", "translate(0," + innerHeight + ")");
 
-        xAxisObj.call(xAxis)
+        xAxisObj
           .append("text")
             .attr("class", "chart-label")
             .attr("fill", "#000")
@@ -140,13 +140,14 @@ this.areaChart = function(svg, settings) {
             .attr("text-anchor", "end")
             .text(settings.x.label);
       }
+      xAxisObj.call(xAxis);
 
       if (yAxisObj.empty()) {
         yAxisObj = chartInner.append("g")
           .attr("class", "y axis")
           .attr("aria-hidden", "true");
 
-        yAxisObj.call(yAxis)
+        yAxisObj
           .append("text")
             .attr("class", "chart-label")
             .attr("fill", "#000")
@@ -156,6 +157,7 @@ this.areaChart = function(svg, settings) {
             .attr("text-anchor", "end")
             .text(settings.y.label);
       }
+      yAxisObj.call(yAxis);
     },
     drawTable = function() {
       var sett = this.settings,

dist/line.js
@@ -128,7 +128,7 @@ this.lineChart = function(svg, settings) {
         .attr("aria-hidden", "true")
         .attr("transform", "translate(0," + innerHeight + ")");
 
-        xAxisObj.call(xAxis)
+        xAxisObj
           .append("text")
             .attr("class", "chart-label")
             .attr("fill", "#000")
@@ -137,13 +137,14 @@ this.lineChart = function(svg, settings) {
             .attr("text-anchor", "end")
             .text(settings.x.label);
       }
+      xAxisObj.call(xAxis);
 
       if (yAxisObj.empty()) {
         yAxisObj = chartInner.append("g")
           .attr("class", "y axis")
           .attr("aria-hidden", "true");
 
-        yAxisObj.call(yAxis)
+        yAxisObj
           .append("text")
             .attr("class", "chart-label")
             .attr("fill", "#000")
@@ -153,6 +154,7 @@ this.lineChart = function(svg, settings) {
             .attr("text-anchor", "end")
             .text(settings.y.label);
       }
+      yAxisObj.call(yAxis);
     },
     drawTable = function() {
       var sett = this.settings,

dist/scatter.js
@@ -198,7 +198,7 @@ this.scatterChart = function(svg, settings) {
         .attr("aria-hidden", "true")
         .attr("transform", "translate(0," + innerHeight + ")");
 
-        xAxisObj.call(xAxis)
+        xAxisObj
           .append("text")
             .attr("class", "chart-label")
             .attr("fill", "#000")
@@ -207,13 +207,14 @@ this.scatterChart = function(svg, settings) {
             .attr("text-anchor", "end")
             .text(settings.x.label);
       }
+      xAxisObj.call(xAxis);
 
       if (yAxisObj.empty()) {
         yAxisObj = chartInner.append("g")
           .attr("class", "y axis")
           .attr("aria-hidden", "true");
 
-        yAxisObj.call(yAxis)
+        yAxisObj
           .append("text")
             .attr("class", "chart-label")
             .attr("fill", "#000")
@@ -223,6 +224,7 @@ this.scatterChart = function(svg, settings) {
             .attr("text-anchor", "end")
             .text(settings.y.label);
       }
+      yAxisObj.call(yAxis);
     },
     drawTable = function() {
       var sett = this.settings,

src/area/area.js
@@ -131,7 +131,7 @@ this.areaChart = function(svg, settings) {
         .attr("aria-hidden", "true")
         .attr("transform", "translate(0," + innerHeight + ")");
 
-        xAxisObj.call(xAxis)
+        xAxisObj
           .append("text")
             .attr("class", "chart-label")
             .attr("fill", "#000")
@@ -140,13 +140,14 @@ this.areaChart = function(svg, settings) {
             .attr("text-anchor", "end")
             .text(settings.x.label);
       }
+      xAxisObj.call(xAxis);
 
       if (yAxisObj.empty()) {
         yAxisObj = chartInner.append("g")
           .attr("class", "y axis")
           .attr("aria-hidden", "true");
 
-        yAxisObj.call(yAxis)
+        yAxisObj
           .append("text")
             .attr("class", "chart-label")
             .attr("fill", "#000")
@@ -156,6 +157,7 @@ this.areaChart = function(svg, settings) {
             .attr("text-anchor", "end")
             .text(settings.y.label);
       }
+      yAxisObj.call(yAxis);
     },
     drawTable = function() {
       var sett = this.settings,

src/line/line.js
@@ -128,7 +128,7 @@ this.lineChart = function(svg, settings) {
         .attr("aria-hidden", "true")
         .attr("transform", "translate(0," + innerHeight + ")");
 
-        xAxisObj.call(xAxis)
+        xAxisObj
           .append("text")
             .attr("class", "chart-label")
             .attr("fill", "#000")
@@ -137,13 +137,14 @@ this.lineChart = function(svg, settings) {
             .attr("text-anchor", "end")
             .text(settings.x.label);
       }
+      xAxisObj.call(xAxis);
 
       if (yAxisObj.empty()) {
         yAxisObj = chartInner.append("g")
           .attr("class", "y axis")
           .attr("aria-hidden", "true");
 
-        yAxisObj.call(yAxis)
+        yAxisObj
           .append("text")
             .attr("class", "chart-label")
             .attr("fill", "#000")
@@ -153,6 +154,7 @@ this.lineChart = function(svg, settings) {
             .attr("text-anchor", "end")
             .text(settings.y.label);
       }
+      yAxisObj.call(yAxis);
     },
     drawTable = function() {
       var sett = this.settings,

src/scatter/scatter.js
@@ -198,7 +198,7 @@ this.scatterChart = function(svg, settings) {
         .attr("aria-hidden", "true")
         .attr("transform", "translate(0," + innerHeight + ")");
 
-        xAxisObj.call(xAxis)
+        xAxisObj
           .append("text")
             .attr("class", "chart-label")
             .attr("fill", "#000")
@@ -207,13 +207,14 @@ this.scatterChart = function(svg, settings) {
             .attr("text-anchor", "end")
             .text(settings.x.label);
       }
+      xAxisObj.call(xAxis);
 
       if (yAxisObj.empty()) {
         yAxisObj = chartInner.append("g")
           .attr("class", "y axis")
           .attr("aria-hidden", "true");
 
-        yAxisObj.call(yAxis)
+        yAxisObj
           .append("text")
             .attr("class", "chart-label")
             .attr("fill", "#000")
@@ -223,6 +224,7 @@ this.scatterChart = function(svg, settings) {
             .attr("text-anchor", "end")
             .text(settings.y.label);
       }
+      yAxisObj.call(yAxis);
     },
     drawTable = function() {
       var sett = this.settings,