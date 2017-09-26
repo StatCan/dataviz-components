89a652f2bb85f768431b1739ea336d55b4a33cb3
Matt Leduc <Matt Leduc>
2017-09-26 13:59:13 -0400

    added the core code

---
+++ src/chord/graph.js
@@ -0,0 +1,64 @@
+(function(extend) {
+var defaults = {
+  margin: {
+    top: 10,
+    right: 10,
+    bottom: 30,
+    left: 50
+  },
+  aspectRatio: 16 / 9,
+  width: 600
+};
+
+this.chordChart = function(svg, settings) {
+  var mergedSettings = extend(true, {}, defaults, settings),
+    outerWidth = mergedSettings.width,
+    outerHeight = Math.ceil(outerWidth / mergedSettings.aspectRatio),
+    innerHeight = mergedSettings.innerHeight = outerHeight - mergedSettings.margin.top - mergedSettings.margin.bottom,
+    innerWidth = mergedSettings.innerWidth = outerWidth - mergedSettings.margin.left - mergedSettings.margin.right,
+    chartInner = svg.select("g"),
+    dataLayer = chartInner.select(".data"),
+    draw = function() {
+      var sett = this.settings,
+        data = (sett.filterData && typeof sett.filterData === "function") ?
+          sett.filterData.call(sett, sett.data) : sett.data;
+
+    },
+    rtnObj, process;
+
+  rtnObj = {
+    settings: mergedSettings,
+    svg: svg
+  };
+
+  svg
+    .attr("viewBox", "0 0 " + outerWidth + " " + outerHeight)
+    .attr("preserveAspectRatio", "xMidYMid meet")
+    .attr("role", "img")
+    .attr("aria-label", mergedSettings.altText);
+
+  if (chartInner.empty()) {
+    chartInner = svg.append("g")
+      .attr("transform", "translate(" + mergedSettings.margin.left + "," + mergedSettings.margin.top + ")");
+  }
+
+  process = function() {
+    draw.apply(rtnObj);
+    if (mergedSettings.datatable === false) return;
+    d3.stcExt.addIEShim(svg, outerHeight, outerWidth);
+  };
+  if (!mergedSettings.data) {
+    d3.json(mergedSettings.url, function(error, data) {
+      mergedSettings.data = data;
+      process();
+    });
+  } else {
+    process();
+  }
+
+  return rtnObj;
+};
+
+})(jQuery.extend, jQuery);
+
+
